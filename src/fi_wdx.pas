ed6cefbd9eb2696769ca8d2c7fa7b1f4d1e9cb7f
Daniel Plakhotich <daniel.plakhotich@gmail.com>
2020-01-19 22:34:17 +0200

    Update copyright year

--- src/fi_wdx.pas
+++ src/fi_wdx.pas
@@ -1,7 +1,7 @@
 {
   Font info WDX plugin.
 
-  Copyright (c) 2015-2017 Daniel Plakhotich
+  Copyright (c) 2015-2020 Daniel Plakhotich
 
   This software is provided 'as-is', without any express or implied
   warranty. In no event will the authors be held liable for any damages
