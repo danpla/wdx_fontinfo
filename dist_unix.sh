6e63f2bc353d06c58ccaa0c7526ec62bf7eca1b3
Daniel Plakhotich <daniel.plakhotich@gmail.com>
2023-02-10 00:21:27 +0100

    Add dist_unix.sh

---
+++ dist_unix.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+VERSION=$(sed -n 's/^version=//p' pluginst.inf)
+
+KERNEL=$(uname -s)
+case $KERNEL in
+    Darwin)
+        SYS=macos
+        ;;
+    *)
+        SYS=$(echo $KERNEL | tr '[:upper:]' '[:lower:]')
+        ;;
+esac
+
+zip \
+    "wdx_fontinfo_${VERSION}_${SYS}.zip" \
+    CHANGES \
+    LICENSE \
+    README.md \
+    fontinfo.wdx \
+    fontinfo.wdx64 \
+    pluginst.inf
